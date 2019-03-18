44767b4914bf606937926fa163b398748661751e
tarekElHajji <tarek.hajji.93@gmail.com>
2019-03-18 23:52:50 +0100

    modify register page

--- JobOffers/Models/AccountViewModels.cs
+++ JobOffers/Models/AccountViewModels.cs
@@ -65,6 +65,11 @@ namespace identity.Models
     public class RegisterViewModel
     {
         [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
