1f6f82b1e9c4ea36fe2624358fd20a779c2d13bc
tarekElHajji <tarek.hajji.93@gmail.com>
2019-03-25 11:53:21 +0100

    add roles

---
+++ JobOffers/Controllers/RoleController.cs
@@ -0,0 +1,99 @@
+﻿using identity.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobOffers.Controllers
+{
+    public class RoleController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Role
+        public ActionResult Index()
+        {
+            return View(db.Roles.ToList());
+        }
+
+        // GET: Role/Details/5
+        public ActionResult Details(string id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var role = db.Roles.Find(id);
+            if(role == null)
+            {
+                return HttpNotFound();
+            }
+            return View(role);
+        }
+
+        // GET: Role/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Role/Create
+        [HttpPost]
+        public ActionResult Create(IdentityRole role)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Roles.Add(role);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(role);
+        }
+
+        // GET: Role/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Role/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Role/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Role/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ JobOffers/Models/RolesViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JobOffers.Models
+{
+    public class RolesViewModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
