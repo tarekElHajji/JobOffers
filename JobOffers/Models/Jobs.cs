d8c56310813f1f75c80d15fe1fcdbf9f10a9a836
tarekElHajji <tarek.hajji.93@gmail.com>
2019-04-11 23:32:21 +0100

    modify jobContent : add CKEditor

--- JobOffers/Models/Jobs.cs
+++ JobOffers/Models/Jobs.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace JobOffers.Models
 {
@@ -17,6 +18,7 @@ namespace JobOffers.Models
         public string JobTitle { get; set; }
 
         [Required]
+        [AllowHtml]
         [Display(Name ="Job Content")]
         public string JobContent { get; set; }
 
